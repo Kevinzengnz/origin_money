98acb653608a853ace69b083a112476cd37205b8
Kevin Zeng <kevinzeng.nz@gmail.com>
2024-05-20 21:04:40 +1200

    Updated Test1

--- consent/infinite-popn/test1.jl
+++ consent/infinite-popn/test1.jl
@@ -2,5 +2,5 @@ include("helper_funcs.jl")
 
 println("TEST SIMPLE POPULATION")
 
-calc_stable_w_1species(name2strategy("-r+,+g-|-r+,+g-"); VERBOSE=true);
+calc_stable_w_1species(name2strategy("-r+,+g-|-r+,+g-|-r+,+g-"); VERBOSE=true);
 
