1dda77687df43f9b4aaada1bc39dfa9a820bf493
Kevin Zeng <kevinzeng.nz@gmail.com>
2024-05-22 10:31:47 +1200

    Updated Test2

--- consent/infinite-popn/test2.jl
+++ consent/infinite-popn/test2.jl
@@ -2,7 +2,7 @@ include("helper_funcs.jl")
 
 println("SHOULD MATCH:")
 
-testStrat = name2strategy("000,+g-|-r+,+g-")
+testStrat = name2strategy("000,+g-|-r+,+g-|-r+,+g-")
 println("calc with 1 species code:\n---------------------------")
 calc_stable_w_1species(testStrat; VERBOSE=true);
 
@@ -14,5 +14,5 @@ calc_stable_w_2species(testStrat, testStrat; rhoA=0.5, VERBOSE=true);
 
 println("TEST OF A MIXTURE:")
 println("====================")
-calc_stable_w_2species(name2strategy("000,+g-|-r+,+g-"), name2strategy("-r+,+g-|-r+,+g-"); rhoA=0.99, VERBOSE=true);
+calc_stable_w_2species(name2strategy("000,+g-|-r+,+g-|-r+,+g-"), name2strategy("-r+,+g-|-r+,+g-|-r+,+g-"); rhoA=0.99, VERBOSE=true);
 
